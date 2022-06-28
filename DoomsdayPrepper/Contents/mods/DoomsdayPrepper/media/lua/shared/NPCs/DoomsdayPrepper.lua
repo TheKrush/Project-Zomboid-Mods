101b5b4a0e7530620d0defe66650d6f6e7550fe3
Keith Yates <krush@lostminions.org>
2022-06-28 00:50:50 -0700

    changing the starting xp

--- DoomsdayPrepper/Contents/mods/DoomsdayPrepper/media/lua/shared/NPCs/DoomsdayPrepper.lua
+++ DoomsdayPrepper/Contents/mods/DoomsdayPrepper/media/lua/shared/NPCs/DoomsdayPrepper.lua
@@ -8,16 +8,16 @@ BaseGameCharacterDetails.DoDoomsdayProfession = function()
     doomsdayprepper:addFreeTrait("Marksman");
 
     -- XP Boosts
-    doomsdayprepper:addXPBoost(Perks.Strength, 3)
-    doomsdayprepper:addXPBoost(Perks.Fitness, 3)
+    doomsdayprepper:addXPBoost(Perks.Strength, 5)
+    doomsdayprepper:addXPBoost(Perks.Fitness, 5)
 
-    doomsdayprepper:addXPBoost(Perks.Sprinting, 2)
-    doomsdayprepper:addXPBoost(Perks.Nimble, 2)
-    doomsdayprepper:addXPBoost(Perks.Lightfoot, 2)
-    doomsdayprepper:addXPBoost(Perks.Sneak, 2)
+    doomsdayprepper:addXPBoost(Perks.Sprinting, 5)
+    doomsdayprepper:addXPBoost(Perks.Nimble, 5)
+    doomsdayprepper:addXPBoost(Perks.Lightfoot, 5)
+    doomsdayprepper:addXPBoost(Perks.Sneak, 5)
 
     doomsdayprepper:addXPBoost(Perks.Axe, 2)
-    doomsdayprepper:addXPBoost(Perks.Maintenance, 2)
+    doomsdayprepper:addXPBoost(Perks.Maintenance, 5)
     doomsdayprepper:addXPBoost(Perks.SmallBlunt, 3)
     doomsdayprepper:addXPBoost(Perks.SmallBlade, 3)
     doomsdayprepper:addXPBoost(Perks.Aiming, 3)
@@ -31,6 +31,7 @@ BaseGameCharacterDetails.DoDoomsdayProfession = function()
     doomsdayprepper:addXPBoost(Perks.Mechanics, 3)
     doomsdayprepper:addXPBoost(Perks.MetalWelding, 3)
     doomsdayprepper:addXPBoost(Perks.PlantScavenging, 2)
+    doomsdayprepper:addXPBoost(Perks.Tailoring, 1)
     doomsdayprepper:addXPBoost(Perks.Trapping, 2)
     doomsdayprepper:addXPBoost(Perks.Woodwork, 3)
 

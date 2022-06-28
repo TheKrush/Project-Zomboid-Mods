41322184810a466d48c8f67244747c91fc63d5cf
Keith Yates <krush@lostminions.org>
2022-06-28 13:02:26 -0700

    Update DoomsdayPrepper.lua

--- DoomsdayPrepper/Contents/mods/DoomsdayPrepper/media/lua/shared/NPCs/DoomsdayPrepper.lua
+++ DoomsdayPrepper/Contents/mods/DoomsdayPrepper/media/lua/shared/NPCs/DoomsdayPrepper.lua
@@ -2,26 +2,26 @@ require("MainCreationMethods");
 
 BaseGameCharacterDetails.DoDoomsdayProfession = function()
 
-    local doomsdayprepper = ProfessionFactory.addProfession("doomsdayprepper", "Doomsday Prepper", "profession_doomsdayprepper2", 100);
+    local doomsdayprepper = ProfessionFactory.addProfession("doomsdayprepper", "Doomsday Prepper", "profession_doomsdayprepper2", 125);
 
     -- ???
     doomsdayprepper:addFreeTrait("Marksman");
 
     -- XP Boosts
-    doomsdayprepper:addXPBoost(Perks.Strength, 5)
-    doomsdayprepper:addXPBoost(Perks.Fitness, 5)
+    doomsdayprepper:addXPBoost(Perks.Fitness, 3)
+    doomsdayprepper:addXPBoost(Perks.Strength, 3)
 
-    doomsdayprepper:addXPBoost(Perks.Sprinting, 5)
-    doomsdayprepper:addXPBoost(Perks.Nimble, 5)
-    doomsdayprepper:addXPBoost(Perks.Lightfoot, 5)
-    doomsdayprepper:addXPBoost(Perks.Sneak, 5)
+    doomsdayprepper:addXPBoost(Perks.Lightfoot, 3)
+    doomsdayprepper:addXPBoost(Perks.Nimble, 3)
+    doomsdayprepper:addXPBoost(Perks.Sneak, 3)
+    doomsdayprepper:addXPBoost(Perks.Sprinting, 3)
 
-    doomsdayprepper:addXPBoost(Perks.Axe, 2)
-    doomsdayprepper:addXPBoost(Perks.Maintenance, 5)
-    doomsdayprepper:addXPBoost(Perks.SmallBlunt, 3)
-    doomsdayprepper:addXPBoost(Perks.SmallBlade, 3)
     doomsdayprepper:addXPBoost(Perks.Aiming, 3)
+    doomsdayprepper:addXPBoost(Perks.Axe, 2)
+    doomsdayprepper:addXPBoost(Perks.Maintenance, 3)
     doomsdayprepper:addXPBoost(Perks.Reloading, 2)
+    doomsdayprepper:addXPBoost(Perks.SmallBlade, 3)
+    doomsdayprepper:addXPBoost(Perks.SmallBlunt, 3)
 
     doomsdayprepper:addXPBoost(Perks.Cooking, 3)
     doomsdayprepper:addXPBoost(Perks.Doctor, 3)
@@ -31,7 +31,7 @@ BaseGameCharacterDetails.DoDoomsdayProfession = function()
     doomsdayprepper:addXPBoost(Perks.Mechanics, 3)
     doomsdayprepper:addXPBoost(Perks.MetalWelding, 3)
     doomsdayprepper:addXPBoost(Perks.PlantScavenging, 2)
-    doomsdayprepper:addXPBoost(Perks.Tailoring, 1)
+    doomsdayprepper:addXPBoost(Perks.Tailoring, 2)
     doomsdayprepper:addXPBoost(Perks.Trapping, 2)
     doomsdayprepper:addXPBoost(Perks.Woodwork, 3)
 

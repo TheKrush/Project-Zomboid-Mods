71fcebfeba4635076aace03c2e025f6408761460
TheKrush <krusho@gmail.com>
2024-04-16 19:53:52 -0700

    Update BSSSB_Item_DisplayNames.lua

--- BetterSorting_SkillBooksMultiPatch/Contents/mods/BetterSorting_SkillBooksMultiPatch/media/lua/client/BSSSB_Item_DisplayNames.lua
+++ BetterSorting_SkillBooksMultiPatch/Contents/mods/BetterSorting_SkillBooksMultiPatch/media/lua/client/BSSSB_Item_DisplayNames.lua
@@ -2,8 +2,10 @@ if getActivatedMods():contains("ItemTweakerAPI") then
 	require("ItemTweaker_Core");
 else return end
 
-TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging1","DisplayName","Scavenging Vol.1 Beginner");
-TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging2","DisplayName","Scavenging Vol.2 Intermediate");
-TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging3","DisplayName","Scavenging Vol.3 Advanced");
-TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging4","DisplayName","Scavenging Vol.4 Expert");
-TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging5","DisplayName","Scavenging Vol.5 Master");
+if getActivatedMods():contains("ScavengingSkillFixed") then
+	TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging1","DisplayName","Scavenging Vol.1 Beginner");
+	TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging2","DisplayName","Scavenging Vol.2 Intermediate");
+	TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging3","DisplayName","Scavenging Vol.3 Advanced");
+	TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging4","DisplayName","Scavenging Vol.4 Expert");
+	TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging5","DisplayName","Scavenging Vol.5 Master");
+end

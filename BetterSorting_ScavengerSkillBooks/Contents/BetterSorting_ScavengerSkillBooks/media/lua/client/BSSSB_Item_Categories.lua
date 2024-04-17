7c187a13fd0889ba4c7e5733febc76bc702f4451
TheKrush <krusho@gmail.com>
2024-04-16 19:20:36 -0700

    BetterSorting_ScavengingSkillBooks

---
+++ BetterSorting_ScavengerSkillBooks/Contents/BetterSorting_ScavengerSkillBooks/media/lua/client/BSSSB_Item_Categories.lua
@@ -0,0 +1,9 @@
+if getActivatedMods():contains("ItemTweakerAPI") then
+	require("ItemTweaker_Core");
+else return end
+
+TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging1","DisplayCategory","LitS");
+TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging2","DisplayCategory","LitS");
+TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging3","DisplayCategory","LitS");
+TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging4","DisplayCategory","LitS");
+TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging5","DisplayCategory","LitS");

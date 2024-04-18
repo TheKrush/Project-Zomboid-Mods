bea52428ed625cd923d60bdcf8f9b1ea9867561e
TheKrush <krusho@gmail.com>
2024-04-17 18:41:56 -0700

    support renaming DrivingSkill books

--- BetterSorting_SkillBooksMultiPatch/Contents/mods/BetterSorting_SkillBooksMultiPatch/media/lua/client/BSSSB_Item_DisplayNames.lua
+++ BetterSorting_SkillBooksMultiPatch/Contents/mods/BetterSorting_SkillBooksMultiPatch/media/lua/client/BSSSB_Item_DisplayNames.lua
@@ -2,6 +2,14 @@ if getActivatedMods():contains("ItemTweakerAPI") then
 	require("ItemTweaker_Core");
 else return end
 
+if getActivatedMods():contains("DrivingSkill") then
+	TweakItem("DrivingSkill.DrivingSkill_BookDriving1","DisplayName","Driving Vol.1 Beginner");
+	TweakItem("DrivingSkill.DrivingSkill_BookDriving2","DisplayName","Driving Vol.2 Intermediate");
+	TweakItem("DrivingSkill.DrivingSkill_BookDriving3","DisplayName","Driving Vol.3 Advanced");
+	TweakItem("DrivingSkill.DrivingSkill_BookDriving4","DisplayName","Driving Vol.4 Expert");
+	TweakItem("DrivingSkill.DrivingSkill_BookDriving5","DisplayName","Driving Vol.5 Master");
+end
+
 if getActivatedMods():contains("ScavengingSkillFixed") then
 	TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging1","DisplayName","Scavenging Vol.1 Beginner");
 	TweakItem("ScavengerSkill.ScavengerSkill_BookScavenging2","DisplayName","Scavenging Vol.2 Intermediate");

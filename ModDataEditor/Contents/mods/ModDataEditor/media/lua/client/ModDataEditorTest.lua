3a74ac0b8f7d2d4c54a974e7595053d90fc225ac
TheKrush <krusho@gmail.com>
2024-04-25 20:25:04 -0700

    Update ModDataEditorTest.lua

--- ModDataEditor/Contents/mods/ModDataEditor/media/lua/client/ModDataEditorTest.lua
+++ ModDataEditor/Contents/mods/ModDataEditor/media/lua/client/ModDataEditorTest.lua
@@ -15,14 +15,39 @@ function dumpModData()
     -- Attempt to resolve the player using the helper method.
     local player = getPlayer();
 
-    print("Mod Data ("..username.."): ", dump(player:getModData()));
+    print("Mod Data ("..username..")")
+    print(dump(player:getModData()))
 
     if (player:getAccessLevel() == "admin") then
         if getActivatedMods():contains("DynamicTraits") then
-            player:getModData().DTisMelancholic = false;
-            player:getModData().DTisNervousWreck = false;
+            player:getModData().MDE_isMelancholic = false;
+            player:getModData().MDE_isNervousWreck = false;
         end
     end
 end
 
-Events.OnGameTimeLoaded.Add(dumpModData)
+-- OnPlayerUpdate Main Method to call others
+function MDE_OnPlayerUpdateMain(player)
+end
+Events.OnPlayerUpdate.Add(MDE_OnPlayerUpdateMain);
+
+-- EveryOneMinutes Main Method to call others
+function MDE_EveryOneMinuteMain()
+end
+Events.EveryOneMinute.Add(MDE_EveryOneMinuteMain);
+
+-- EveryTenMinutes Main Method to call others
+function MDE_EveryTenMinutesMain()
+end
+Events.EveryTenMinutes.Add(MDE_EveryTenMinutesMain);
+
+-- EveryHours Main Method to call others
+function MDE_EveryHoursMain()
+    dumpModData()
+end
+Events.EveryHours.Add(MDE_EveryHoursMain);
+
+-- EveryDays Main Method to call others
+function MDE_EveryDaysMain()
+end
+Events.EveryDays.Add(MDE_EveryDaysMain);

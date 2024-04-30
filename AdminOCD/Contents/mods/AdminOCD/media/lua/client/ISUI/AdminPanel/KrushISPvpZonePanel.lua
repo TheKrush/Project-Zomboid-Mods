1204cb62c390d713e9bf13d1e851b16532ea332f
TheKrush <krusho@gmail.com>
2024-04-30 16:30:16 -0700

    attempt #3+

--- AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISPvpZonePanel.lua
+++ AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISPvpZonePanel.lua
@@ -1,59 +1,52 @@
-require "client/ISUI/AdminPanel/ISPvpZonePanel"
 
-AdminOCD.ISPvpZonePanel_initialise = function()
-    local original_initialise = ISPvpZonePanel["initialise"]
-    ISPvpZonePanel["initialise"] = function()
-        original_initialise();
+local original_initialise = ISPvpZonePanel.initialise
+ISPvpZonePanel.initialise = function()
+    original_initialise();
 
-        local btnWid = 100
-        local btnHgt = math.max(25, FONT_HGT_SMALL + 3 * 2)
-        local btnHgt2 = FONT_HGT_SMALL + 2 * 2
-        local padBottom = 10
+    local btnWid = 100
+    local btnHgt = math.max(25, FONT_HGT_SMALL + 3 * 2)
+    local btnHgt2 = FONT_HGT_SMALL + 2 * 2
+    local padBottom = 10
 
-        self.changeTitle = ISButton:new(self.nonPvpList.x + self.addZone.width + 5, self.addZone.y, 70, btnHgt2, getText("IGUI_PlayerStats_ChangeTitle"), self, ISPvpZonePanel.onClick);
-        self.changeTitle.internal = "CHANGETITLE";
-        self.changeTitle:initialise();
-        self.changeTitle:instantiate();
-        self.changeTitle.borderColor = self.buttonBorderColor;
-        self:addChild(self.changeTitle);
-        self.changeTitle.enable = false;
+    self.changeTitle = ISButton:new(self.nonPvpList.x + self.addZone.width + 5, self.addZone.y, 70, btnHgt2, getText("IGUI_PlayerStats_ChangeTitle"), self, ISPvpZonePanel.onClick);
+    self.changeTitle.internal = "CHANGETITLE";
+    self.changeTitle:initialise();
+    self.changeTitle:instantiate();
+    self.changeTitle.borderColor = self.buttonBorderColor;
+    self:addChild(self.changeTitle);
+    self.changeTitle.enable = false;
 
-        self:setHeight(self.no:getBottom() + padBottom)
+    self:setHeight(self.no:getBottom() + padBottom)
 
-        self:populateList();
+    self:populateList();
+end
+
+local original_populateList = ISPvpZonePanel.populateList
+ISPvpZonePanel.populateList = function()
+    self.nonPvpList:clear();
+
+    -- copy then sort the list
+    local nonPvpZones=NonPvpZone.getAllZones()
+    table.sort(nonPvpZones, function(a,b)
+        if a.getTitle() < b.getTitle() then return true end
+    end)
+
+    -- now add each item
+    for i=0,nonPvpZones:size()-1 do
+        local zone = nonPvpZones:get(i);
+        self.nonPvpList:addItem(zone:getTitle(), zone);
     end
 end
 
-AdminOCD.ISPvpZonePanel_populateList = function()
-    local original_populateList = ISPvpZonePanel["populateList"]
-    ISPvpZonePanel["populateList"] = function()
-        self.nonPvpList:clear();
-
-        -- copy then sort the list
-        local nonPvpZones=NonPvpZone.getAllZones()
-        table.sort(nonPvpZones, function(a,b)
-            if a.getTitle() < b.getTitle() then return true end
-        end)
-
-        -- now add each item
-        for i=0,nonPvpZones:size()-1 do
-            local zone = nonPvpZones:get(i);
-            self.nonPvpList:addItem(zone:getTitle(), zone);
-        end
-    end
-end
-
-AdminOCD.ISPvpZonePanel_onClick = function()
-    local original_onClick = ISPvpZonePanel["onClick"]
-    ISPvpZonePanel["onClick"] = function(button)
-        original_onClick(button) -- call the original one
-        if button.internal == "CHANGETITLE" then
-            local modal = ISTextBox:new(self.x + 200, 200, 280, 180, getText("IGUI_PvpZone_ChangeTitle"), self.selectedZone:getTitle(), nil, ISPvpZonePanel.onChangeTitle);
-            modal.ui = self;
-            modal.selectedZone = self.selectedZone;
-            modal:initialise();
-            modal:addToUIManager();
-        end
+local original_onClick = ISPvpZonePanel.onClick
+ISPvpZonePanel.onClick = function(button)
+    original_onClick(button) -- call the original one
+    if button.internal == "CHANGETITLE" then
+        local modal = ISTextBox:new(self.x + 200, 200, 280, 180, getText("IGUI_PvpZone_ChangeTitle"), self.selectedZone:getTitle(), nil, ISPvpZonePanel.onChangeTitle);
+        modal.ui = self;
+        modal.selectedZone = self.selectedZone;
+        modal:initialise();
+        modal:addToUIManager();
     end
 end
 
@@ -65,12 +58,3 @@ function ISPvpZonePanel:onChangeTitle(button)
         button.parent.ui:populateList();
     end
 end
-
--- OnGameStart adds callback for OnGameStart global event.
-AdminOCD.ISPvpZonePanelOnGameStart = function()
-    AdminOCD.ISPvpZonePanel_initialise()
-    AdminOCD.ISPvpZonePanel_populateList()
-    AdminOCD.ISPvpZonePanel_onClick()
-end
-
-Events.OnGameStart.Add(AdminOCD.ISPvpZonePanelOnGameStart);
--- AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISSafehousesList.lua
+++ AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISSafehousesList.lua
@@ -1,27 +1,17 @@
-require "client/ISUI/AdminPanel/ISSafehousesList"
 
-AdminOCD.ISSafehousesList_populateList = function()
-    local original_populateList = ISSafehousesList["populateList"]
-    ISSafehousesList["populateList"] = function()
-        self.datas:clear();
+local original_populateList = ISSafehousesList.populateList
+ISSafehousesList.populateList = function()
+    self.datas:clear();
 
-        -- copy then sort the list
-        local safeHouses=SafeHouse.getSafehouseList()
-        table.sort(safeHouses, function(a,b)
-            if a.getTitle() < b.getTitle() then return true end
-        end)
+    -- copy then sort the list
+    local safeHouses=SafeHouse.getSafehouseList()
+    table.sort(safeHouses, function(a,b)
+        if a.getTitle() < b.getTitle() then return true end
+    end)
 
-        -- now add each item
-        for i=0,safeHouses:size()-1 do
-            local safeHouse = safeHouses:get(i);
-            self.datas:addItem(safeHouse:getTitle(), safeHouse);
-        end
+    -- now add each item
+    for i=0,safeHouses:size()-1 do
+        local safeHouse = safeHouses:get(i);
+        self.datas:addItem(safeHouse:getTitle(), safeHouse);
     end
 end
-
--- OnGameStart adds callback for OnGameStart global event.
-AdminOCD.ISSafehousesListOnGameStart = function()
-    AdminOCD.ISSafehousesList_populateList()
-end
-
-Events.OnGameStart.Add(AdminOCD.ISSafehousesListOnGameStart);

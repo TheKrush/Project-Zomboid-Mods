db09667e57e484d274a984a5d858f2ae7ec500aa
TheKrush <krusho@gmail.com>
2024-05-02 20:32:33 -0700

    Update KrushISPvpZonePanel.lua

--- AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISPvpZonePanel.lua
+++ AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISPvpZonePanel.lua
@@ -67,9 +67,9 @@ function ISPvpZonePanel:onChangeTitle(button)
 
         local newTitle = button.parent.entry:getText();
         local x1 = math.floor(math.min(button.parent.selectedZone:getX(), button.parent.selectedZone:getX2()));
-        local y1 = math.floor(math.min(button.parent.selectedZone.getY(), button.parent.selectedZone:getY2()));
+        local y1 = math.floor(math.min(button.parent.selectedZone:getY(), button.parent.selectedZone:getY2()));
         local x2 = math.floor(math.max(button.parent.selectedZone:getX(), button.parent.selectedZone:getX2()) + 1);
-        local y2 = math.floor(math.max(button.parent.selectedZone.getY(), button.parent.selectedZone:getY2()) + 1);
+        local y2 = math.floor(math.max(button.parent.selectedZone:getY(), button.parent.selectedZone:getY2()) + 1);
 
         -- remove old zone
         NonPvpZone.removeNonPvpZone(oldTitle);

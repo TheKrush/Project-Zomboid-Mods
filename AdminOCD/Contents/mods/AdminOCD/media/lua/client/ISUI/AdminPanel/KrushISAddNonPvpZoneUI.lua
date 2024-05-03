3031337ea34b8d4575ce6c1bd88b7f8e31c2dd24
TheKrush <krusho@gmail.com>
2024-05-02 19:37:55 -0700

    Update KrushISAddNonPvpZoneUI.lua

--- AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISAddNonPvpZoneUI.lua
+++ AdminOCD/Contents/mods/AdminOCD/media/lua/client/ISUI/AdminPanel/KrushISAddNonPvpZoneUI.lua
@@ -14,13 +14,13 @@ ISAddNonPvpZoneUI.onClick = function(self, button)
             self:setVisible(false);
             self:removeFromUIManager();
 
-            local title = self.titleEntry:getInternalText()
-            local X1 = math.floor(math.min(self.startingX, self.player.getX()));
-            local Y1 = math.floor(math.min(self.startingY, self.player.getY()));
-            local X2 = math.floor(math.max(self.startingX, self.player.getX()) + 1);
-            local Y2 = math.floor(math.max(self.startingY, self.player.getY()) + 1);
+            local title = self.titleEntry:getInternalText();
+            local x1 = math.floor(math.min(self.startingX, self.player.getX()));
+            local y1 = math.floor(math.min(self.startingY, self.player.getY()));
+            local x2 = math.floor(math.max(self.startingX, self.player.getX()) + 1);
+            local y2 = math.floor(math.max(self.startingY, self.player.getY()) + 1);
 
-            NonPvpZone.addNonPvpZone(title, X1, Y1, X2, Y2)
+            NonPvpZone.addNonPvpZone(title, x1, y1, x2, y2);
         else
             return;
         end
